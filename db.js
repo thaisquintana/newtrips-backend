6f47c628f6633b25ea3fecbecb444ecdf106dfc7
Thais Quintana <thaisquintanap@gmail.com>
2024-04-28 02:09:18 -0300

    added port to env file

--- db.js
+++ db.js
@@ -2,25 +2,23 @@ import 'dotenv/config'
 
 import postgres from 'postgres'
 
-let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID, PORT } = process.env;
 
 const sql = postgres({
-    host: PGHOST,
-    database: PGDATABASE,
-    username: PGUSER,
-    password: PGPASSWORD,
-    port: 5432,
-    ssl: 'require',
-    connection: {
-      options: `project=${ENDPOINT_ID}`,
-    },
-  });  
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: PORT,
+  ssl: 'require',
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
+  },
+});
 
 async function getPgVersion() {
-    const result = await sql`select version()`;
-    console.log(result);
-  }
-  
-  getPgVersion();
+  const result = await sql`select version()`;
+  console.log(result);
+}
 
-export default sql
+getPgVersion();
