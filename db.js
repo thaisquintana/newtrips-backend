d85f0f037a438071130deef8c27e80e401297edf
Thais Quintana <thaisquintanap@gmail.com>
2024-04-28 02:25:12 -0300

    declare port

--- db.js
+++ db.js
@@ -2,14 +2,14 @@ import 'dotenv/config'
 
 import postgres from 'postgres'
 
-let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID, PORT } = process.env;
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
 
 export const sql = postgres({
   host: PGHOST,
   database: PGDATABASE,
   username: PGUSER,
   password: PGPASSWORD,
-  port: PORT,
+  port: 5432,
   ssl: 'require',
   connection: {
     options: `project=${ENDPOINT_ID}`,
